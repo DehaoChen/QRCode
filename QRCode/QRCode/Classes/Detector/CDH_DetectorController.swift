3fc62267513b8f66f24ed6d59bd4e098b52072ea
chendehao <958689927@qq.com>
2016-08-06 01:27:22 +0800

    分别给三个 storyboard 添加控制器

---
+++ QRCode/QRCode/Classes/Detector/CDH_DetectorController.swift
@@ -0,0 +1,35 @@
+//
+//  CDH_DetectorController.swift
+//  QRCode
+//
+//  Created by chendehao on 16/8/6.
+//  Copyright © 2016年 CDH. All rights reserved.
+//
+
+import UIKit
+
+class CDH_DetectorController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
